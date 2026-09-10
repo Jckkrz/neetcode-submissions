dafadc1d460d9e50e2c1382082553ff3c2f57abf
Jack Hardy <jack.hardy.2024@uni.strath.ac.uk>
2026-09-10 14:52:18 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            alphabet[s.charAt(i) - 'a'] += 1;
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            alphabet[t.charAt(i) - 'a'] -= 1;
+        }
+
+        for(int num : alphabet){
+            if(num != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
